3f0386d68426e4f52eaca068527f3f0949e05a13
BathlaNeffi <neffybathla@gmail.com>
2023-12-24 05:15:25 +0530

    created the download CSV using fast-csv package

--- controller/students_controller.js
+++ controller/students_controller.js
@@ -1,4 +1,6 @@
 const Student= require('../models/student');
+const csv = require("fast-csv");
+const fs = require("fs");
 module.exports.studentsPage=async(req,res)=>{
     try {
         if(req.isAuthenticated()){
@@ -25,3 +27,71 @@ module.exports.addStudents= async(req,res)=>{
         return res.redirect('back');
     }
 }
+
+
+
+//Download a complete CSV of all the data of students
+module.exports.downloadCSV = async (req, res) => {
+    try {
+      let students = await Student.find().populate("interviews");
+      const csvStream = csv.format({ headers: true });
+  
+      if (!fs.existsSync("assets/files/export")) {
+        if (!fs.existsSync("assets/files")) {
+          fs.mkdirSync("assets/files/");
+        }
+  
+        if (!fs.existsSync("assets/files/export/")) {
+          fs.mkdirSync("./assets/files/export/");
+        }
+      }
+  
+      const writeableStream = fs.createWriteStream(
+        "assets/files/export/students.csv"
+      );
+  
+      csvStream.pipe(writeableStream);
+  
+      writeableStream.on("finish", function () {
+        return res.json({
+         downloadURL: `http://localhost:9000/files/export/students.csv`,
+        });
+      });
+      if (students.length > 0) {
+  
+        students.map((student) => {
+  
+          let interviews = student.interviews || [];
+  
+          interviews.map((interview) => {
+  
+            let results = interview.results || [];
+  
+            results.map((result) =>{
+  
+            
+          csvStream.write({
+            Student_id: student._id ? student._id : "-",
+            Student_Name: student.name ? student.name : "-",
+            Student_College: student.college ? student.college : "-",
+            Student_Status: student.status ? student.status : "-",
+            Student_DSAFinalScore: student.DSAFinalScore ? student.DSAFinalScore : "-",
+            Student_WebDFinalScore: student.WebDFinalScore ? student.WebDFinalScore : "-",
+            Student_ReactFinalScore: student.ReactFinalScore ? student.ReactFinalScore : "-",
+            Student_InterviewDate: interview.date ? interview.date : "-",
+            Student_InterviewCompany: interview.company ? interview.company : "-",
+            Student_InterviewResult: result.result ? result.result : "-",
+          });
+        });
+      });
+    });
+  
+      }
+      csvStream.end();
+      writeableStream.end();
+    } catch (err) {
+      console.log(`error in download CSV controller ${err}`);
+      return;
+    }
+  };
+  
--- routes/students.js
+++ routes/students.js
@@ -4,4 +4,5 @@ const studentsController= require('../controller/students_controller');
 
 router.get('/',studentsController.studentsPage);
 router.post('/add-students',studentsController.addStudents);
+router.get('/download-csv',studentsController.downloadCSV);
 module.exports=router;
