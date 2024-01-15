c2dafb63d5e17f1fafc25d6f28f036de6fc59f1e
BathlaNeffi <neffybathla@gmail.com>
2024-01-16 00:34:44 +0530

    Added remote db

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect(`mongodb://localhost:27017/${process.env.PLACEMENT_CELL_DB}`);
+mongoose.connect(`mongodb+srv://${process.env.PLACEMENT_CELL_REMOTE_dB}`);
+// mongoose.connect(`mongodb://localhost:27017/${process.env.PLACEMENT_CELL_DB}`);
 const db=mongoose.connection;
 db.on('error', console.error.bind(console,'Error in connecting to the MongoDB'));
 db.once('open',function(){
